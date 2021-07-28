78e79f4f63528b35a8fed65baba732b4304eb1ec
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-07-28 23:36:04 +0000

    added control feature that allows the user to control the snake using the W, A, S, D keys on the keyboard.

--- input.js
+++ input.js
@@ -1,16 +1,30 @@
+let connection;
 const net = require('net');
 
 const handleUserInput = (key) => {
   if (key === '\u0003') process.exit();
 };
 
-const setupInput = () => {
+const wasd = (key) => {
+  if (key === "w") connection.write("Move: up")
+  else if (key === "a") connection.write("Move: left")
+  else if (key === "s") connection.write("Move: down")
+  else if (key === "d") connection.write("Move: right")
+};
+
+
+const setupInput = (conn) => {
+  connection = conn;
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
  
   stdin.on("data", handleUserInput); 
-  }
+  stdin.on("data", wasd); 
+    
+}
+
 
   module.exports = setupInput;
