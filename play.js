d85b56066026383d597a4e3c9483773efe2ecf8f
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-07-28 22:34:40 +0000

    Used key inputs to be able to terminate the game at any time

--- play.js
+++ play.js
@@ -1,4 +1,18 @@
 const connect = require('./client')
-
 console.log("Connecting ...");
-connect();
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+ 
+  const handleUserInput = (key) => {
+    if (key === '\u0003') process.exit();
+  };
+  stdin.on("data", handleUserInput); 
+  }
+  
+
+setupInput(connect());
+
